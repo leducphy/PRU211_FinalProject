7d14488c8d55efd0cc32db204d8d7c81f6c49cb5
linhnt <linhpro141202@gmail.com>
2023-11-09 02:20:15 +0700

    feature/darkmode

---
+++ Assets/Scripts/WorldTimeConstants.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldTimeConstants : MonoBehaviour
+{
+    public const int MinutesInDay = 1440;
+}
---
+++ Assets/Scripts/WorldTimeDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_Text))]
+public class WorldTimeDisplay : MonoBehaviour
+{
+    private TMP_Text _text;
+    private int _dayCount;
+    private float _timer;
+    private float _interval = 30f;
+    private void Awake()
+    {
+        _text = GetComponent<TMP_Text>();
+        _dayCount = 0;
+    }
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        int increment = Mathf.FloorToInt(_timer / _interval);
+        if (increment > 0)
+        {
+            _timer -= increment * _interval;
+            _dayCount += increment;
+        }
+        _text.SetText("Day: " + _dayCount);
+    }
+}
